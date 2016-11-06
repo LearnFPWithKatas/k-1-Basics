03af0e415db0a4c240a527dab8ef9a4b77c406d8
Partho P. Das <this@parthopdas.com>
2016-11-07 05:38:32 +0700

    Step 4. 5 returns "Buzz"

--- App.Tests/FizzBuzz.Tests.fs
+++ App.Tests/FizzBuzz.Tests.fs
@@ -7,5 +7,6 @@ open global.Xunit
 [<InlineData(1, "1")>]
 [<InlineData(2, "2")>]
 [<InlineData(3, "Fizz")>]
+[<InlineData(5, "Buzz")>]
 let ``Given number returns expected label`` inputNumber expectedLabel =
     FizzBuzz inputNumber |> should equal expectedLabel
--- App/FizzBuzz.fs
+++ App/FizzBuzz.fs
@@ -2,4 +2,5 @@
 
 let FizzBuzz number = 
     if number % 3 = 0 then "Fizz"
+    else if number % 5 = 0 then "Buzz"
     else number.ToString()
