b675745d60081a0fa63473e46a0e9742d94861a4
Partho P. Das <this@parthopdas.com>
2016-11-07 05:53:06 +0700

    Step 12. Refactor to unify semantics by using Option type

--- App/FizzBuzz.fs
+++ App/FizzBuzz.fs
@@ -4,17 +4,20 @@ let FizzBuzzWithPipeline ruleFactor ruleLabel data =
     let number, label = data
     match number with
     | n when n % ruleFactor = 0 -> 
-        let newLabel = label + ruleLabel
-        (number, newLabel)
+        let newLabel = 
+            match label with
+            | Some l -> l + ruleLabel
+            | None -> ruleLabel
+        (number, Some newLabel)
     | _ -> data
 
 let ConvertToLabel(number, label) = 
     match label with
-    | l when l <> "" -> l
-    | _ -> number.ToString()
+    | Some l -> l
+    | None -> number.ToString()
 
 let FizzBuzz number = 
-    (number, "")
+    (number, None)
     |> FizzBuzzWithPipeline 3 "Fizz"
     |> FizzBuzzWithPipeline 5 "Buzz"
     |> ConvertToLabel
