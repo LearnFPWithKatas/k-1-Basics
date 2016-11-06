3259de2850facdc6bc1c82d82c977bcee16d1b11
Partho P. Das <this@parthopdas.com>
2016-11-07 05:49:19 +0700

    Step 9. Refactor to remove mutability by introducing pipeline

--- App/FizzBuzz.fs
+++ App/FizzBuzz.fs
@@ -1,15 +1,16 @@
 ﻿module App.FizzBuzz
 
-let FizzBuzzWithRules rules number = 
-    let mutable printed = false
-    let mutable label = ""
-    for ruleFactor, ruleLabel in rules do
-        if number % ruleFactor = 0 then 
-            printed <- true
-            label <- label + ruleLabel
-    if printed then label
-    else number.ToString()
+let FizzBuzzWithPipeline ruleFactor ruleLabel data = 
+    let number, label = data
+    if number % ruleFactor = 0 then 
+        let newLabel = label + ruleLabel
+        (number, newLabel)
+    else data
 
 let FizzBuzz number = 
-    FizzBuzzWithRules [ (3, "Fizz")
-                        (5, "Buzz") ] number
+    (number, "")
+    |> FizzBuzzWithPipeline 3 "Fizz"
+    |> FizzBuzzWithPipeline 5 "Buzz"
+    |> fun (n, l) -> 
+        if l <> "" then l
+        else n.ToString()
